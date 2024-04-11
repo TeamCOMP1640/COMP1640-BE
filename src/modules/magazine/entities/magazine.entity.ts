2280d12bf1949617a33a85f31f9cdcb1f4a8ff8b
Thang <thanglhgcd201414@fpt.edu.vn>
2024-04-12 06:05:28 +0700

    COMP1640-73-BE: Bonus information

--- src/modules/magazine/entities/magazine.entity.ts
+++ src/modules/magazine/entities/magazine.entity.ts
@@ -1,3 +1,4 @@
+import { AcademicEntity } from '@app/modules/academic/entities';
 import { FacultyEntity } from '@app/modules/falcuties/entities';
 import { Column, Entity, ManyToOne, PrimaryGeneratedColumn } from 'typeorm';
 
@@ -9,7 +10,7 @@ export class MagazineEntity {
   @Column({ type: 'varchar', length: 50 })
   name: string;
 
-  @Column({ type: 'varchar', length: 500 })
+  @Column({ type: 'varchar', length: 500, nullable: true })
   description: string;
 
   @Column({ type: 'date' })
@@ -17,4 +18,7 @@ export class MagazineEntity {
 
   @ManyToOne(() => FacultyEntity, (faculty) => faculty.magazines)
   faculty: FacultyEntity;
+
+  @ManyToOne(() => AcademicEntity, (faculty) => faculty.magazines)
+  academic: AcademicEntity;
 }
